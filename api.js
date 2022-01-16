120e9c7d182d1248f72c1afec26c172adc98c084
ssoonnaa <sonalisharma7724@gmail.com>
2022-01-17 03:49:23 +0530

    17commit

--- api.js
+++ api.js
@@ -1,6 +1,18 @@
 const express = require ('express');
 const pool = require ('./database')
 const router = express.Router ();
+router.get('/todo/rollno',async function(req,res){
+  try {
+        const myquery = 'SELECT `mylist` FROM `todolist`.`todotable`';
+        const rows = await pool.query(myquery,req.params.mylist);
+        console.log("MY DB" , rows);
+        //res.status(200).json({rollno:req.params.rollno})
+        res.status(200).json({mylist:rows})
+
+    } catch (error) {
+        res.status(400).send("MY DB ERROR" + error.message);       
+    }
+});
 router.post('/todo/register', async function(req,res){
     try {
       const {mylist}=req.body;
@@ -11,18 +23,19 @@ router.post('/todo/register', async function(req,res){
       const myquery= 'INSERT INTO todotable (mylist) VALUES (?)';
       const result = await pool.query(myquery,[mylist]);
       res.status(200).json({todolist:result.insertlist});
-      console.log("inseerted");
+      console.log("inserted");
       } 
       catch (error) {
         res.status(400).send(error.message);
       }
 
 });
-router.delete('/todo/remove/:rollno',async function(req,res){
+router.delete('/todo/delete/:mylist',async function(req,res){
     try{
-      const myquery = 'DELETE FROM `todolist`.`todotable` WHERE  `no`=?;';
+      const myquery = 'DELETE FROM `todolist`.`todotable` WHERE  `mylist`= ?';
       const rpl = await pool.query (myquery,req.params.rollno);
-      res.status(200).json({todolist1:rpl.deleteno});
+      res.status(200).json({todolist1:rpl.deletemylist});
+      console.log("deleted");
     }
     catch(error){
       res.status(400).send(error.message);
--- todotable.sql
+++ todotable.sql
@@ -1,7 +1,6 @@
+
 CREATE TABLE `todotable` (
-	`no` INT(10) NOT NULL AUTO_INCREMENT,
-	`mylist` VARCHAR(5000) NOT NULL COLLATE 'utf8mb3_general_ci',
-	PRIMARY KEY (`no`) USING BTREE
+	`mylist` VARCHAR(5000) NOT NULL COLLATE 'utf8mb3_general_ci'
 )
 COLLATE='utf8mb3_general_ci'
 ENGINE=InnoDB
